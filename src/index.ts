27175524d5abdd670c72378186dcf6da1be19cb3
Orta Therox <orta.therox@gmail.com>
2018-09-29 12:41:26 -0400

    Merge master

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-export function handler(event, context) {
-  context.succeed("hello world")
+import * as OctoKit from "@octokit/rest"
+import { filepathContentsMapToUpdateGitHubBranch } from "memfs-or-file-map-to-github-branch"
+
+export async function handler(event, context) {
+  await makeAPR()
+  return { hello: "world" }
+}
+
+const makeAPR = async () => {
+  const settings = {
+    owner: "danger",
+    repo: "danger-js",
+    fullBranchReference: "heads/a_new_branch",
+    message: "Adds some new stuff",
+  }
+
+  const token = process.env.DANGER_GITHUB_API_TOKEN || process.env.GITHUB_API_TOKEN
+  if (!token) {
+    throw new Error("No API token set up for this function")
+  }
+
+  const octokit = new OctoKit()
+  octokit.authenticate({
+    type: "token",
+    token,
+  })
+
+  const fileMap = {
+    "README.md": "hello, world",
+  }
+
+  await filepathContentsMapToUpdateGitHubBranch(octokit, fileMap, settings)
 }
